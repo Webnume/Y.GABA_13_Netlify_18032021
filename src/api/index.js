732fa1672c3ea932db9d8274067ecb1d8fa0e54e
Yves G <contact@webnumerik.fr>
2023-01-03 18:45:49 +0100

    fix: host change

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 // export const url = "http://localhost:3001/api/v1";
-export const url = "https://argentbankbackend-app.herokuapp.com/api/v1";
+// export const url = "https://argentbankbackend-app.herokuapp.com/api/v1";
+export const url = "https://project-13-bank-api.onrender.com";
+
 
 export const setHeaders = () => {
   const getToken =
